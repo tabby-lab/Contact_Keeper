bf1b14bd478c87c0ee08996b46cc4b2946790930
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-22 17:46:56 -0500

    added routes folder with files of auth,contact,and user

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res)=> res.json({ msg: 'Welcome to the ContactKeeper API...' }));
+app.get('/', (req,res)=> 
+res.json({ msg: 'Welcome to the ContactKeeper API...' }));
 
 const PORT = process.env.PORT || 5000;
 
