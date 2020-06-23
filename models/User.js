a7821309493cb456c0dd15014d646849ccc3b53c
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-23 08:58:29 -0500

    validated using express validater for user

--- models/User.js
+++ models/User.js
@@ -5,16 +5,22 @@ const UserSchema = mongoose.Schema({
         type: String,
         required:true
     },
-    name:{
+    email:{
+        type: String,
+        required:true,
+        unique: true
+    },
+    password:{
         type: String,
         required:true
     },
-    name:{
-        type: String,
-        required:true
-    },
-    name:{
-        type: String,
-        required:true
-    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
 });
+
+module.exports = mongoose.model('user', UserSchema);
+
+//i need contact and user model
+//with mongoose,when dealing with database resourses you need to have a model for each resourse
