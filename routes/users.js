cca9bbcdd9b7746f93811d67150b41ccd1333b9e
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-23 10:29:11 -0500

    added user and token, now need to login with current user

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../models/User')
 
@@ -43,8 +45,18 @@ router.post('/', [
 
         await user.save();
 
-        res.send('User saved');
+        const payload = {
+            user: {
+                id: user.id
 
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        })
     } catch (err) {
         console.log(err.message);
         res.status(500).send('Server Error');
