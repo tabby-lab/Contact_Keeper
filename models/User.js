f04fa7bdb93311ad7bca54052ea91687cd8f028a
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-22 19:40:29 -0500

    started on models with a user schema.not done yet

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require ('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required:true
+    },
+    name:{
+        type: String,
+        required:true
+    },
+    name:{
+        type: String,
+        required:true
+    },
+    name:{
+        type: String,
+        required:true
+    },
+});
