a58e60c98ceea3183c5e9fe8892a089de9e51bf9
Tabby Garcia <tabby.garcia1@gmail.com>
2020-06-26 13:24:32 -0500

    making sure data is correct on postman

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,7 +28,8 @@ router.post('/', [auth, [
         .isEmpty()
 
  ]
- ], async (req, res) => {
+ ],
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
