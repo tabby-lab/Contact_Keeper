089a6f2b166cc842df81d9601421ea2ed16cd176
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-20 12:33:46 -0500

    created login.js and register.js for users. in register js creating states

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+const Register = () => {
+    const[ user, setUser ] = useState({
+        name: '',
+        email:'',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = user;
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Register
