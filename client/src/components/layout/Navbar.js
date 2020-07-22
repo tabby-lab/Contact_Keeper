31c1624c370d54166e76507d7229f3615ede910f
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-22 15:28:26 -0500

    brought in authcontext to navbar and added auth links and guest links

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+
 
 const Navbar = ({ title, icon}) => {
+    const authContext = useContext(AuthContext);
+
+    const { isAuthenticated, logout, user } = authContext;
+
+    const authLinks = (
+        <Fragment>
+            <li>Hello { user && user.name} </li>
+            <a href='#!'>
+                <i className="fas fa-sign-out-alt" />{' '}
+                <span className="hide-sm">Logout</span>
+            </a>
+        </Fragment>
+    );
+    const guestLinks = (
+        <Fragment>
+             <li>
+                <Link to='/register'>Register</Link>
+                </li>
+                <li>
+                <Link to='/login'>Login</Link>
+                </li>
+        </Fragment>
+    );
+
     return (
         <div className="navbar bg-primary">
            <h1>
                 <i className={icon} /> {title}
             </h1> 
             <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                <Link to='/about'>About</Link>
-                </li>
-                <li>
-                <Link to='/register'>Register</Link>
-                </li>
-                <li>
-                <Link to='/login'>Login</Link>
-                </li>
+           {isAuthenticated ? authLinks : guestLinks}
             </ul>
         </div>
     )
@@ -39,3 +54,10 @@ Navbar.defaultProps = {
 export default Navbar;
 
 //rafc functional w hooks
+//if we want a home and about page
+               {/* <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                <Link to='/about'>About</Link>
+                </li> */}
