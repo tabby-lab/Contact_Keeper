e3fb1338caba7158a0023c59519356bd5820e0bc
Tabby Garcia <tabby.garcia1@gmail.com>
2020-07-23 14:25:19 -0500

    ready to deploy

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -29,7 +29,7 @@ case ADD_CONTACT:
         return{
             ...state,
             contacts:state.contacts.map(contact => 
-                contact.id === action.payload.id ? 
+                contact._id === action.payload._id ? 
                 action.payload : contact),
                 loading:false
             }
